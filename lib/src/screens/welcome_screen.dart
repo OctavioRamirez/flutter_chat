1908bf8454a6f48de0548f6ece9103a6f6dcf937
blizzey <visser@elumen.info>
2022-03-10 17:23:46 -0300

    future added to welcome_screen.dart

--- lib/src/screens/welcome_screen.dart
+++ lib/src/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/src/Widgets/app_button.dart';
 import 'package:flutter_chat/src/Widgets/app_icon.dart';
@@ -13,30 +15,42 @@ class welcomeScreen extends StatefulWidget {
 class _welcomeScreenState extends State<welcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                AppIcon(),
-                SizedBox(
-                  height: 48.0,
-                ),
-                AppButton(
-                    color: Colors.blueAccent,
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    name: 'Log in'),
-                AppButton(
-                    color: Colors.lightBlueAccent,
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/registration');
-                    },
-                    name: 'Registrarse')
-              ],
-            )));
+    return FutureBuilder(future: Firebase.initializeApp()
+        , builder: (context, snapshot) {
+          return Scaffold(
+              body: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      AppIcon(),
+                      SizedBox(
+                        height: 48.0,
+                      ),
+                      AppButton(
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          name: 'Log in'),
+                      AppButton(
+                          color: Colors.lightBlueAccent,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/registration');
+                          },
+                          name: 'Registrarse')
+                    ],
+                  )));
+        });
+
   }
+}
+
+Future<DocumentSnapshot> getData() async {
+  await Firebase.initializeApp();
+  return await FirebaseFirestore.instance
+      .collection("users")
+      .doc("docID")
+      .get();
 }
