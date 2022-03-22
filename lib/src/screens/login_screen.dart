39164cd3ad9296e4144efb1b948a129bcaae5630
blizzeratrops <bro.visser@gmail.com>
2022-03-22 20:00:40 -0300

    spinner (legacy tho)

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -3,6 +3,12 @@ import 'package:flutter_chat/src/Widgets/app_button.dart';
 import 'package:flutter_chat/src/Widgets/app_icon.dart';
 import 'package:flutter_chat/src/Widgets/app_textfield.dart';
 import 'package:flutter_chat/src/services/authentication.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+/// This library does not support sound null safety methods
+/// thus it does not compile well on flutter 2.10+
+/// but we can bypass this running it like this
+/// flutter run --no-sound-null-safety
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -17,6 +23,7 @@ class _LoginScreenState extends State<LoginScreen> {
       TextEditingController();
   final FocusNode _focusNodeEmail = FocusNode();
   final FocusNode _focusNodePassword = FocusNode();
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -27,63 +34,73 @@ class _LoginScreenState extends State<LoginScreen> {
     _passwordFieldController.dispose();
   }
 
+  void toggleSpinner(bool status) {
+    setState(() {
+      _isLoading = status;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                AppIcon(),
-                SizedBox(
-                  height: 48.0,
-                ),
-                AppTextfield(
-                  inputText: "Ingresar Email",
-                  obscureText: false,
-                  onChanged: (value) {
-                    _email = value;
-                  },
-                  controller: _emailFieldController,
-                  focusNode: _focusNodeEmail,
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                AppTextfield(
-                  focusNode: _focusNodePassword,
-                  inputText: "Ingresar Contraseña",
-                  obscureText: true,
-                  onChanged: (value) {
-                    _password = value;
-                  },
-                  controller: _passwordFieldController,
-                ),
-                SizedBox(
-                  height: 23.0,
-                ),
-                AppButton(
-                    color: Colors.blueAccent,
-                    onPressed: () async {
-                      var user = await Authenticator()
-                          .logInUser(email: _email, password: _password);
-                      if (user != null) {
-                        Navigator.pushNamed(context, '/chat');
-                        // _emailFieldController.text = '';
-                        _passwordFieldController.text = '';
+        body: ModalProgressHUD(
+      inAsyncCall: _isLoading,
+      child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              AppIcon(),
+              SizedBox(
+                height: 48.0,
+              ),
+              AppTextfield(
+                inputText: "Ingresar Email",
+                obscureText: false,
+                onChanged: (value) {
+                  _email = value;
+                },
+                controller: _emailFieldController,
+                focusNode: _focusNodeEmail,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              AppTextfield(
+                focusNode: _focusNodePassword,
+                inputText: "Ingresar Contraseña",
+                obscureText: true,
+                onChanged: (value) {
+                  _password = value;
+                },
+                controller: _passwordFieldController,
+              ),
+              SizedBox(
+                height: 23.0,
+              ),
+              AppButton(
+                  color: Colors.blueAccent,
+                  onPressed: () async {
+                    toggleSpinner(true);
+                    var user = await Authenticator()
+                        .logInUser(email: _email, password: _password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, '/chat');
+                      // _emailFieldController.text = '';
+                      _passwordFieldController.text = '';
 
-                        if (_emailFieldController.text.isNotEmpty) {
-                          FocusScope.of(context)
-                              .requestFocus(_focusNodePassword);
-                        } else {
-                          FocusScope.of(context).requestFocus(_focusNodeEmail);
-                        }
+                      if (_emailFieldController.text.isNotEmpty) {
+                        FocusScope.of(context).requestFocus(_focusNodePassword);
+                      } else {
+                        FocusScope.of(context).requestFocus(_focusNodeEmail);
                       }
-                    },
-                    name: 'Log in')
-              ],
-            )));
+                      toggleSpinner(false);
+                    }
+                  },
+                  name: 'Log in')
+            ],
+          )),
+    ));
   }
 }
